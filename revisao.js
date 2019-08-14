917e324bf7b36b5ba59febe8e2548a8a26665fba
anderson <am33zhick@gmail.com>
2019-08-14 20:32:32 -0300

    MediaAula3

---
+++ revisao.js
@@ -0,0 +1,5 @@
+var nota1 = 9;
+var nota2 = 3;
+var nota3 = 7;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A sua media é" + media);
---
+++ revisao.ts
@@ -0,0 +1,7 @@
+let nota1 : number = 9   
+let nota2 : number = 3   
+let nota3 : number = 7 
+let media : number = (nota1 + nota2 + nota3)/3
+
+console.log ("A sua media é"   +   media)
+
