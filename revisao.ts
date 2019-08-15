187950a6348062678a4419cea1957573fc9454c4
am33zhick <am33zhick@gmail.com>
2019-08-14 22:13:49 -0300

    MediaAula3media valor (função)

--- revisao.ts
+++ revisao.ts
@@ -1,13 +1,13 @@
 let n1 : number = 9   
 let n2 : number = 3   
-let n3 : number = 7 
+//let n3 : number = 7 
 //let media : number = (nota1 + nota2 + nota3)/3/
 
-let fmedia = function(nota1:number,nota2:number,nota3:number):number
-{
-    return(nota1 + nota2 + nota3)/3
-}
-
-console.log (`A sua media é:   +   ${fmedia(n1 , n2 , n3)}`)
+//let fmedia = function(nota1:number,nota2:number,nota3:number):number
+//{
+  //  return(nota1 + nota2 + nota3)/3/
+//}
+let fmedia = (nota1:number,nota2:number,nota3:number=7)=>(nota1 + nota2 + nota3)/3    // modelo simplificado /
+console.log (`A sua media é:   +   ${fmedia(n1 , n2 )}`)
 //console.log ("A sua media é: "  +   fmedia(n1 , n2 , n3))  outra forma de fazer a chamada/
 
